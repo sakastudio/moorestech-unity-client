e70f2aea67e5874d3ebaa3d19c5c96a46ad3d156
sakastudio <sakastudio100@gmail.com>
2021-07-07 11:21:45 +0900

    実際にタイルマップをクリックしてデータを受信する処理

--- Assets/Scripts/Unity/TileMapTest.cs
+++ Assets/Scripts/Unity/TileMapTest.cs
@@ -12,6 +12,8 @@ public class TileMapTest : MonoBehaviour
 {
     [SerializeField] private Tilemap machinesTilemap;
     [SerializeField] private TileBase machinesTile;
+
+    private List<PutReservation> _putReservationList = new List<PutReservation>();
     // Start is called before the first frame update
     void Start()
     {
@@ -28,22 +30,73 @@ public class TileMapTest : MonoBehaviour
             // Z軸修正
             position.z = 10f;
             // マウス位置座標をスクリーン座標からワールド座標に変換する
-            var screen = Camera.main.ScreenToWorldPoint(position);
+            var tile = machinesTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(position));
             
             var send = new List<byte>();
             send.AddRange(ByteArrayConverter.ToByteArray((short)1));
             send.AddRange(ByteArrayConverter.ToByteArray(0));
             send.AddRange(ByteArrayConverter.ToByteArray((short)0));
-            send.AddRange(ByteArrayConverter.ToByteArray(Mathf.RoundToInt(screen.x)));
-            send.AddRange(ByteArrayConverter.ToByteArray(Mathf.RoundToInt(screen.y)));
+            send.AddRange(ByteArrayConverter.ToByteArray(tile.x));
+            send.AddRange(ByteArrayConverter.ToByteArray(tile.y));
             send.AddRange(ByteArrayConverter.ToByteArray(Guid.Empty));
             send.AddRange(ByteArrayConverter.ToByteArray(Guid.Empty));
         
             PacketHandler.SendMessages(send.ToArray());
         }
+
+        foreach (var putReservation in _putReservationList)
+        {
+            machinesTilemap.SetTile(new Vector3Int(putReservation.X,putReservation.Y,0),machinesTile);
+        }
+
+        _putReservationList.Clear();
+    }
+
+    private void FixedUpdate()
+    {
+        GetData();
+    }
+
+    public void GetData()
+    {
+        for (int i = -10; i <= 10; i+=10)
+        {
+            for (int j = -10; j <= 10; j+=10)
+            {
+                SendInstallationRequest(i, j);
+            }
+        }
+    }
+
+    void SendInstallationRequest(int chunkX, int chunkY)
+    {
+        var payload = new List<byte>();
+        //パケットIDの挿入
+        short id = 2;
+        payload.AddRange(ByteArrayConverter.ToByteArray(id));
+        payload.Add(0);
+        payload.Add(0);
+        payload.AddRange(ByteArrayConverter.ToByteArray(chunkX));
+        payload.AddRange(ByteArrayConverter.ToByteArray(chunkY));
+        
+        PacketHandler.SendMessages(payload.ToArray());
     }
     void InstallationCoordinateResponseEvent(int x, int y, int id, Guid guid)
     {
-        machinesTilemap.SetTile(new Vector3Int(x,y,0),machinesTile);
+        _putReservationList.Add(new PutReservation(x,y,0));
+    }
+}
+
+class PutReservation
+{
+    public readonly int X;
+    public readonly int Y;
+    public readonly int id;
+
+    public PutReservation(int x, int y, int id)
+    {
+        X = x;
+        Y = y;
+        this.id = id;
     }
 }
