6ed42e92921ccaf5a0db4ba6ed79d31956f668a0
sakastudio <sakastudio100@gmail.com>
2021-07-23 00:09:59 +0900

    コントローラー作成

--- Assets/Scripts/UnityCode/UI/UIDisplayOverallControl.cs
+++ Assets/Scripts/UnityCode/UI/UIDisplayOverallControl.cs
@@ -7,9 +7,32 @@ namespace UnityCode.UI
     public class UIDisplayOverallControl : MonoBehaviour
     {
          [SerializeField] private List<UIObjectData> uiObjectDataList;
+         private UIObjectData _openedUIObject;
         void Update()
         {
-        
+            //何かしらUIが開いていたら閉じるかどうかの判定処理
+            if (_openedUIObject.UIObject.IsShowUI())
+            {
+                //開かれているUIのキーが押されたら閉じる
+                if (Input.GetKeyDown(_openedUIObject.keyCode))
+                {
+                    _openedUIObject.UIObject.HideUI();
+                }
+            }
+            //UIが開かれてなければ登録されているUIのどれかのキーが押されるまで待機
+            else
+            {
+                foreach (var uiObjectData in uiObjectDataList)
+                {
+                    //特定のキーが押されていたらそのuiを開く
+                    if (Input.GetKeyDown(uiObjectData.keyCode))
+                    {
+                        uiObjectData.UIObject.ShowUI();
+                        _openedUIObject = uiObjectData;
+                        break;
+                    }
+                }
+            }
         }
     }
 
