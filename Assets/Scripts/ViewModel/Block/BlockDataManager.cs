9ff237ca25fc6bf5e0a56ec87650877a32ec9385
sakastudio <sakastudio100@gmail.com>
2021-08-10 23:51:12 +0900

    リクエスト処理

--- Assets/Scripts/ViewModel/Block/BlockDataManager.cs
+++ Assets/Scripts/ViewModel/Block/BlockDataManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Const;
 using Network;
 using UnityEngine;
 
@@ -16,7 +17,35 @@ namespace ViewModel.Block
 
         private void SendBlockRequestPacket(Vector2Int start,Vector2Int end)
         {
-            
+            //リクエスト処理
+            int startChunkX = Mathf.RoundToInt(start.x)/
+                ConstData.DefaultChunkSize * ConstData.DefaultChunkSize;
+            int endChunkX = Mathf.RoundToInt(end.x)/
+                ConstData.DefaultChunkSize * ConstData.DefaultChunkSize;
+            int startChunkY = Mathf.RoundToInt(start.y)/
+                ConstData.DefaultChunkSize * ConstData.DefaultChunkSize;
+            int endChunkY = Mathf.RoundToInt(end.y)/
+                ConstData.DefaultChunkSize * ConstData.DefaultChunkSize;
+            for (int i = startChunkX; i < endChunkX; i+=ConstData.DefaultChunkSize)
+            {
+                for (int j = startChunkY; j < endChunkY; j+=ConstData.DefaultChunkSize)
+                {
+                    SendInstallationRequest(i, j);
+                }
+            }
+        }
+        void SendInstallationRequest(int chunkX, int chunkY)
+        {
+            var payload = new List<byte>();
+            //パケットIDの挿入
+            short id = 2;
+            payload.AddRange(ByteArrayConverter.ToByteArray(id));
+            payload.Add(0);
+            payload.Add(0);
+            payload.AddRange(ByteArrayConverter.ToByteArray(chunkX));
+            payload.AddRange(ByteArrayConverter.ToByteArray(chunkY));
+        
+            PacketHandler.SendMessages(payload.ToArray());
         }
         
         private int[,] CollectBlockData(Vector2Int start,Vector2Int end)
