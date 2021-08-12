738012da2a08c12d1a4f425221d083afd97f9d9b
sakastudio <sakastudio100@gmail.com>
2021-08-12 09:55:54 +0900

    イベント追加

--- Assets/Scripts/ViewModel/Block/BlockDataManager.cs
+++ Assets/Scripts/ViewModel/Block/BlockDataManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Network;
+using Network.ReceivePacket;
 using Network.Util;
 using UnityEngine;
 using Util.Block;
@@ -10,13 +12,14 @@ namespace ViewModel.Block
     public class BlockDataManager
     {
         private static readonly Dictionary<Coordinate,BlockData> CoordinateDictionary = new Dictionary<Coordinate, BlockData>();
-        public int[,] GetBlocks(Vector2Int start,Vector2Int end)
+        public static int[,] GetBlocks(Vector2Int start,Vector2Int end)
         {
+            if(!isSubscribe) BlockCoordinateReceive.SubscribeEvent(ReceiveBlockDataEvent);
             SendBlockRequestPacket(start, end);
             return CollectBlockData(start, end);
         }
 
-        private void SendBlockRequestPacket(Vector2Int start,Vector2Int end)
+        private static void SendBlockRequestPacket(Vector2Int start,Vector2Int end)
         {
             //リクエスト処理
             int startChunkX = Mathf.RoundToInt(start.x)/
@@ -35,7 +38,7 @@ namespace ViewModel.Block
                 }
             }
         }
-        void SendInstallationRequest(int chunkX, int chunkY)
+        static void SendInstallationRequest(int chunkX, int chunkY)
         {
             var payload = new List<byte>();
             //パケットIDの挿入
@@ -49,7 +52,7 @@ namespace ViewModel.Block
             PacketHandler.SendMessages(payload.ToArray());
         }
         
-        private int[,] CollectBlockData(Vector2Int start,Vector2Int end)
+        private static int[,] CollectBlockData(Vector2Int start,Vector2Int end)
         {
             
             var xSize = Math.Abs(start.x - end.x);
@@ -71,6 +74,43 @@ namespace ViewModel.Block
                 }
             }
             return blocks;
+        }
+
+        private static bool isSubscribe = false;
+        private static void ReceiveBlockDataEvent(Dictionary<Coordinate, BlockData> blockData)
+        {
+            var startChunk = new Vector2Int(
+                blockData.First().Key.x /
+                ConstData.DefaultChunkSize * 
+                ConstData.DefaultChunkSize,
+                blockData.First().Key.y /
+                ConstData.DefaultChunkSize * 
+                ConstData.DefaultChunkSize);
+            for (int i = startChunk.x; i < startChunk.x + ConstData.DefaultChunkSize; i++)
+            {
+                for (int j = startChunk.x; j < startChunk.x + ConstData.DefaultChunkSize; j++)
+                {
+                    var c = new Coordinate {x = i, y = j};
+                    BlockData insert;
+                    if (blockData.ContainsKey(c))
+                    {
+                        insert = blockData[c];
+                    }
+                    else
+                    {
+                        insert = new BlockData {id = -1, intId = Int32.MinValue};
+                    }
+                    
+                    if (CoordinateDictionary.ContainsKey(c))
+                    {
+                        blockData[c] = insert;
+                    }
+                    else
+                    {
+                        blockData.Add(c,insert);
+                    }
+                }
+            }
         }
     }
 }
