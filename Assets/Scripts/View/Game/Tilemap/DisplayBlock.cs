33f1087f39f0818d5b47dd1ddfcedee22b41bef5
sakastudio <sakastudio100@gmail.com>
2021-08-14 14:58:48 +0900

    とりあえずクラス作った

---
+++ Assets/Scripts/View/Game/Tilemap/DisplayBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace View.Game.Tilemap
+{
+    public class DisplayBlock : MonoBehaviour
+    {
+        private void Update()
+        {
+            
+        }
+    }
+}
