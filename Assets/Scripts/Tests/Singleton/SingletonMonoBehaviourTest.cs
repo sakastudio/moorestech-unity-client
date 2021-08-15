a593b76226cd984d61ff461e915abbb31f074115
sakastudio <sakastudio100@gmail.com>
2021-08-16 00:33:02 +0900

    テストコード書いた

--- Assets/Scripts/Tests/Singleton/SingletonMonoBehaviourTest.cs
+++ Assets/Scripts/Tests/Singleton/SingletonMonoBehaviourTest.cs
@@ -1,13 +1,57 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Util;
 
 namespace Tests.Singleton
 {
     public class SingletonMonoBehaviourTest
     {
-        [Test]
-        public void NotInSceneTest()
+        [UnityTest]
+        public IEnumerator NotInSceneTest()
         {
-            
+            try
+            {
+                var a = TestSingletonClass.Instance;
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.Pass();
+            }
+            yield return null;
         }
+
+        [UnityTest]
+        public IEnumerator OneInSceneTest()
+        {
+            new GameObject().AddComponent<TestSingletonClass>();
+            var a = TestSingletonClass.Instance;
+            Assert.Pass();
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TwoInSceneTest()
+        {
+            new GameObject().AddComponent<TestSingletonClass>();
+            new GameObject().AddComponent<TestSingletonClass>();
+            try
+            {
+                var a = TestSingletonClass.Instance;
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.Pass();
+            }
+            yield return null;
+        }
+    }
+    public class TestSingletonClass : SingletonMonoBehaviour<TestSingletonClass>
+    {
+        
     }
 }
