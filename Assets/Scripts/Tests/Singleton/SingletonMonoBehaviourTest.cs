8acbcf042e0a225d05bb1aa59c35c67312facca3
sakastudio <sakastudio100@gmail.com>
2021-08-16 00:12:56 +0900

    テスト用アセンブリ作り直し

---
+++ Assets/Scripts/Tests/Singleton/SingletonMonoBehaviourTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Singleton
+{
+    public class SingletonMonoBehaviourTest
+    {
+        [Test]
+        public void NotInSceneTest()
+        {
+            
+        }
+    }
+}
