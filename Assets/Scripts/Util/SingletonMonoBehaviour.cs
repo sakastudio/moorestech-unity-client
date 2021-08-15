f7c69edac7923f602c9c4f5dc8ff702c85777280
sakastudio <sakastudio100@gmail.com>
2021-08-15 23:52:49 +0900

    シングルトンの中身実装

--- Assets/Scripts/Util/SingletonMonoBehaviour.cs
+++ Assets/Scripts/Util/SingletonMonoBehaviour.cs
@@ -4,5 +4,30 @@ namespace Util
 {
     public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var result = FindObjectsOfType<T>();
+                    if (result.Length == 0)
+                    {
+                        Debug.LogError($"{typeof(T).Name}はシーンに存在しません");
+                        return null;
+                    }
+                    if (result.Length <= 2)
+                    {
+                        Debug.LogError($"{typeof(T).Name}はシーンに2以上存在しています");
+                        return null;
+                    }
+                    _instance = result[0];
+                }
+
+                return _instance;
+            }
+        }
     }
 }
