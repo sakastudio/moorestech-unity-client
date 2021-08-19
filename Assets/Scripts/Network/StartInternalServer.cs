48f34685e7e623982d01d76af05e7e1b294b93c7
sakastudio <sakastudio100@gmail.com>
2021-08-20 00:47:05 +0900

    StartInternalServer作った

---
+++ Assets/Scripts/Network/StartInternalServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using industrialization.Core.GameSystem;
+using Util;
+
+namespace Network
+{
+    public class StartInternalServer : SingletonMonoBehaviour<StartServerCommunication>
+    {
+        public void FixedUpdate()
+        {
+            GameUpdate.Update();
+        }
+    }
+}
